bfd6cf6d437400de079e9ad2b2ccc449c4513326
cyq <sion@cyqm2.local>
2024-08-11 17:49:08 +0800

    send message with keys

--- webapp/src/main/java/comcom/controller/SendMessageController.java
+++ webapp/src/main/java/comcom/controller/SendMessageController.java
@@ -1,11 +1,13 @@
 package comcom.controller;
 
 import jakarta.annotation.Resource;
+import org.apache.rocketmq.common.message.MessageConst;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +21,14 @@ public class SendMessageController {
     @Resource
     private StreamBridge streamBridge;
 
-    @RequestMapping("/api/message/send")
-    public ResponseEntity<?> sendMessage() {
-        streamBridge.send("cloudStreamP-out-0",
-                MessageBuilder.withPayload("hello cloud stream " + LocalDateTime.now()).build());
+    @RequestMapping("/api/message/send/{key}")
+    public ResponseEntity<?> sendMessage(@PathVariable(required = false) String key) {
+
+        MessageBuilder<String> messageBuilder = MessageBuilder.withPayload("hello cloud stream " + LocalDateTime.now());
+        if (Objects.nonNull(key)) {
+            messageBuilder.setHeader(MessageConst.PROPERTY_KEYS, key);
+        }
+        streamBridge.send("cloudStreamP-out-0",messageBuilder.build());
 
         return ResponseEntity.ok("ok");
     }
